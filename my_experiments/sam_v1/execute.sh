9f65fbe6326a846afe2b06c3c283eb8e13c43c0b
filipeas <felipealvessampaio@hotmail.com>
2024-11-09 19:53:22 +0000

    update in execute.sh

--- my_experiments/sam_v1/execute.sh
+++ my_experiments/sam_v1/execute.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-# vai pra raiz
-cd ../..
-
-echo "installing requirements..."
-pip install -r requirements.txt
-
-echo "installing Minerva-Dev..."
-cd Minerva-Dev
-pip install .
-
-# volta pra pasta original
-cd -
 python experiment_sam.py --config config_experiment_sam.json
