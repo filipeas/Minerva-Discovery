24c9d08627070aacf312a72ca66d70c3e81368e3
filipeas <felipealvessampaio@hotmail.com>
2025-01-05 17:26:25 +0000

    new experiment 2 - train sam with one seismic facie per step

---
+++ my_experiments/sam_original/exec_experiment_2/data_module.py
@@ -0,0 +1,179 @@
+from minerva.data.datasets.supervised_dataset import SimpleDataset
+from minerva.data.readers.png_reader import PNGReader
+from minerva.data.readers.tiff_reader import TiffReader
+from minerva.transforms.transform import _Transform
+from minerva.data.readers.reader import _Reader
+from torch.utils.data import DataLoader
+import lightning as L
+import torch
+from typing import List, Optional, Tuple
+import numpy as np
+from pathlib import Path
+import random
+import os
+
+class Padding(_Transform):
+    def __init__(self, target_h_size: int, target_w_size: int):
+        self.target_h_size = target_h_size
+        self.target_w_size = target_w_size
+
+    def __call__(self, x: np.ndarray) -> np.ndarray:
+        h, w = x.shape[:2]
+        pad_h = max(0, self.target_h_size - h)
+        pad_w = max(0, self.target_w_size - w)
+        if len(x.shape) == 2:
+            padded = np.pad(x, ((0, pad_h), (0, pad_w)), mode="reflect")
+            padded = np.expand_dims(padded, axis=2)
+            padded = torch.from_numpy(padded).float()
+        else:
+            padded = np.pad(x, ((0, pad_h), (0, pad_w), (0, 0)), mode="reflect")
+            padded = torch.from_numpy(padded).float()
+
+        padded = np.transpose(padded, (2, 0, 1))
+        return padded
+
+class DatasetForSAM(SimpleDataset):
+    def __init__(
+            self, 
+            readers: List[_Reader], 
+            transforms: Optional[_Transform] = None,
+            select_facie:int=0
+    ):
+        super().__init__(readers, transforms)
+        self.select_facie = select_facie # pode ser: 0 (facie mais escura), a 5 (facie mais clara)
+
+        assert (
+            len(self.readers) == 2
+        ), "DatasetForSAM requires exactly 2 readers (image your label)"
+    
+    def __getitem__(self, index: int) -> Tuple[np.ndarray, np.ndarray]:
+        data_readers = []
+        for reader, transform in zip(self.readers, self.transforms):
+            sample = reader[index]
+            if transform is not None:
+                sample = transform(sample)
+            data_readers.append(sample)
+
+        image = data_readers[0]
+        label = data_readers[1]
+
+        # Gera uma máscara binária apenas para a fácie selecionada
+        binary_mask = (label == self.select_facie).to(torch.uint8)
+
+        data = {
+            'image': image,
+            'label': binary_mask,
+            'original_size': (int(image.shape[1]), int(image.shape[2])),
+            'class_id': self.select_facie
+        }
+
+        return data
+
+class DataModule(L.LightningDataModule):
+    def __init__(
+        self,
+        train_path: str,
+        annotations_path: str,
+        transforms: _Transform = None,
+        select_facie:int=0,
+        batch_size: int = 1,
+        data_ratio: float = 1.0,
+        num_workers: int = None,
+    ):
+        super().__init__()
+        self.train_path = Path(train_path)
+        self.annotations_path = Path(annotations_path)
+        self.transforms = transforms
+        self.select_facie = select_facie
+        self.batch_size = batch_size
+        self.data_ratio = data_ratio
+        self.num_workers = (
+            num_workers if num_workers is not None else os.cpu_count()
+        )
+
+        self.datasets = {}
+
+    def setup(self, stage=None):
+        if stage == "fit":
+            train_img_reader = TiffReader(self.train_path / "train")
+            train_label_reader = PNGReader(self.annotations_path / "train")
+
+            # applying ratio
+            num_train_samples = int(len(train_img_reader) * self.data_ratio)
+            if num_train_samples < len(train_img_reader):
+                indices = random.sample(range(len(train_img_reader)), num_train_samples)
+                train_img_reader = [train_img_reader[i] for i in indices]
+                train_label_reader = [train_label_reader[i] for i in indices]
+                
+            train_dataset = DatasetForSAM(
+                readers=[train_img_reader, train_label_reader],
+                transforms=self.transforms,
+                select_facie=self.select_facie
+            )
+
+            val_img_reader = TiffReader(self.train_path / "val")
+            val_label_reader = PNGReader(self.annotations_path / "val")
+            val_dataset = DatasetForSAM(
+                readers=[val_img_reader, val_label_reader],
+                transforms=self.transforms,
+                select_facie=self.select_facie
+            )
+
+            self.datasets["train"] = train_dataset
+            self.datasets["val"] = val_dataset
+
+        elif stage == "test" or stage == "predict":
+            test_img_reader = TiffReader(self.train_path / "test")
+            test_label_reader = PNGReader(self.annotations_path / "test")
+            test_dataset = DatasetForSAM(
+                readers=[test_img_reader, test_label_reader],
+                transforms=self.transforms,
+                select_facie=self.select_facie
+            )
+            self.datasets["test"] = test_dataset
+            self.datasets["predict"] = test_dataset
+
+        else:
+            raise ValueError(f"Invalid stage: {stage}")
+    
+    def custom_collate_fn(self, batch):
+        """
+        Custom collate function for DataLoader to return a list of dictionaries.
+        """
+        return batch 
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.datasets["train"],
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=True,
+            collate_fn=self.custom_collate_fn
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.datasets["val"],
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=False,
+            collate_fn=self.custom_collate_fn
+        )
+
+    def test_dataloader(self):
+        return DataLoader(
+            self.datasets["test"],
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=False,
+            collate_fn=self.custom_collate_fn
+        )
+
+    def predict_dataloader(self):
+        return DataLoader(
+            self.datasets["predict"],
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=False,
+            collate_fn=self.custom_collate_fn
+        )
---
+++ my_experiments/sam_original/exec_experiment_2/readme.md
@@ -0,0 +1,7 @@
+## train with prompt
+1. A model that segment only 1 of N seismic facies:
+2. A model that segment one seismic facie, seeing all of the N facies, separatelly.
+    - PS1: probablly this will works only with prompts.
+    - PS2: apply augmentation in prompts:
+        - use from 1 to 5 points (positive/negative)
+        - use box
