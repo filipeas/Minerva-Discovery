c9ad0e24d548cc553949ffa82873a6e70790cd2f
filipeas <felipealvessampaio@hotmail.com>
2025-01-14 12:39:33 +0000

    add general run_sbatch in root project

---
+++ run_sbatch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --partition=gpulongd       # Nome da partição que você vai usar (ex: gpulongd)
+#SBATCH --account=asml-gpu         # Conta associada
+#SBATCH --job-name=sam_FAS  # Nome do job
+#SBATCH --time=220:00:00           # Tempo máximo para o job (em horas)
+
+# Exibe informações do job para facilitar o debug
+echo "Job ID: $SLURM_JOB_ID"
+echo "Running on node: $(hostname)"
+echo "Allocated GPU(s): $CUDA_VISIBLE_DEVICES"
+
+# Executa o container Singularity com suporte a GPU e roda o script de setup
+singularity exec --nv Singularity.sif \ 
+    python /workspaces/Minerva-Discovery/my_experiments/sam_original/exec_experiment_2/_with_prompt/main.py \ 
+    --config /workspaces/Minerva-Discovery/my_experiments/sam_original/exec_experiment_2/config_experiment_parihaka.json
